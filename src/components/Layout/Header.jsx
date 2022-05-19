e880940465ba59be7e973d324dad9a00104e6c1b
SOOCHUL SON <zerozerobase@gmail.com>
2022-05-20 00:01:40 +0900

    :tada: initial setting

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Header() {
+  return (
+    <>
+      <Head>
+        <title>환자 관리 시스템</title>
+        <meta name="description" content="Patients Management System" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  )
+}
